7988bf8af8b759becfcf6f141053eb30cb1ce61f
citkcaamal <programaciondyn3.mx@sandos.com>
2019-12-31 11:46:04 -0500

    Aplicando estilos en linea a componente react

--- src/components/tarjetaFruta.js
+++ src/components/tarjetaFruta.js
@@ -79,8 +79,18 @@ class TarjetaFruta extends React.Component {
     // }
 
     render() {
+        const hasItems = this.state.cantidad > 0
+        const styles = {
+            border: '1px solid black',
+            marginBottom: '1em',
+            borderRadius: '0.5em',
+            padding: '1em',
+            background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : 'white',
+            color: hasItems ? '#FFF' : '#000',
+            transition: 'all 400ms ease-out'
+        }
         return (
-            <div>
+            <div style={styles}>
                 <h3> {this.props.name} </h3>
                 <hr />
                 <div> Cantidad: {this.state.cantidad} </div>
@@ -95,6 +105,9 @@ class TarjetaFruta extends React.Component {
                 <button onClick={this.limpiar} > Limpiar </button>
 
                 <p> $ {this.props.price}</p>
+                <p>
+                    Total ${this.props.price * this.state.cantidad}
+                </p>
             </div>
         )
     }
