299b20d4afd7efac359348e67442506f281bd5f0
citkcaamal <programaciondyn3.mx@sandos.com>
2019-12-31 10:43:49 -0500

    breve refactorizacion de componentes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  React  from 'react'
+import TarjetaFruta from './components/tarjetaFruta';
+
+const App = () => (
+    <div>
+        {/* props vienen como atributos */}
+        <TarjetaFruta name='sandia' price={2.00} />
+        <TarjetaFruta name='naranja' price={5.00} />
+        <TarjetaFruta name='melon' price={1.00} />
+        <TarjetaFruta name='mango' price={3.00} />
+    </div>
+)
+
+export default App
---
+++ src/components/tarjetaFruta.js
@@ -0,0 +1,103 @@
+import  React  from 'react'
+
+const name = 'Cituk Caamal'
+const user = {
+    name: 'San',
+    age: 28,
+    country: 'mexico'
+}
+// Este es un componente funcional con arrow functions
+// const TarjetaFruta = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//             <h3> {props.name} </h3>
+//             <hr/>
+//             <p> $ {props.price}</p>
+//         </div>
+//     )
+// }
+
+class TarjetaFruta extends React.Component {
+    constructor() {
+        super()
+        // this.agregar = this.agregar.bind(this)
+        // this.quitar = this.quitar.bind(this)
+
+        //inicializadores de propiedad
+        const METHODS = [
+            'agregar',
+            'quitar',
+            'limpiar'
+        ]
+        // igual a lo hecho anterior con bind pero este recorre un array y hace el bind
+        METHODS.forEach((method) => {
+            this[method] = this[method].bind(this)
+        })
+
+        this.state = {
+            cantidad: 25
+        }
+    }
+
+    // state = {
+    //     cantidad: 25
+    // };
+    agregar() {
+        this.setState({
+            cantidad: this.state.cantidad + 1
+        })
+    }
+    quitar() {
+        this.setState({
+            cantidad: this.state.cantidad - 1
+        })
+    }
+
+    limpiar() {
+        this.setState({
+            cantidad: 0
+        })
+    }
+
+    //inicializadores de funciones, obliga a sacar el state del constructor y transforma las funciones de la clase en arrow functions
+    // agregar = () => {
+    //     this.setState({
+    //         cantidad: this.state.cantidad + 1
+    //     })
+    // }
+    // quitar = () => {
+    //     this.setState({
+    //         cantidad: this.state.cantidad - 1
+    //     })
+    // }
+
+    // limpiar = () => {
+    //     this.setState({
+    //         cantidad: 0
+    //     })
+    // }
+
+    render() {
+        return (
+            <div>
+                <h3> {this.props.name} </h3>
+                <hr />
+                <div> Cantidad: {this.state.cantidad} </div>
+                <button
+                    onClick={this.agregar //() =>{
+                        // this.state.cantidad = 100
+                        // this.forceUpdate()
+                        // this.setState({ cantidad: this.state.cantidad + 1 })
+                    } //} 
+                >+ </button>
+                <button onClick={this.quitar} > - </button>
+                <button onClick={this.limpiar} > Limpiar </button>
+
+                <p> $ {this.props.price}</p>
+            </div>
+        )
+    }
+}
+
+export default TarjetaFruta
