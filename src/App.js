be19839b16ca05849fb69f61b8b6fd19db202f74
citkcaamal <programaciondyn3.mx@sandos.com>
2020-01-02 11:44:55 -0500

    mutando el estado de componente con una funcion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import  React  from 'react'
-import TarjetaFruta from './components/TarjetaFruta';
+import  React  , {Component }from 'react'
+// import TarjetaFruta from './components/TarjetaFruta';
 
+// const App = () => (
+//     <div>
+//         {/* props vienen como atributos */}
+//         <TarjetaFruta name='sandia' price={2.00} />
+//         <TarjetaFruta name='naranja' price={5.00} />
+//         <TarjetaFruta name='melon' price={1.00} />
+//         <TarjetaFruta name='mango' price={3.00} />
+//     </div>
+// )
+
+// export default App
+
+class Contador extends Component {
+    state = {
+        video: {
+            title: 'super video',
+            likes: 0
+        }
+    }
+
+    add = () => {
+        this.setState( (state) => ({
+            video:{
+                ...state.video,
+                likes: state.video.likes + 1
+            }
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.state.video.title}</h1>
+                <button onClick={ this.add}>
+                    Clicks: ({this.state.video.likes})
+                </button>
+            </div>
+        )
+    }
+}
 const App = () => (
     <div>
-        {/* props vienen como atributos */}
-        <TarjetaFruta name='sandia' price={2.00} />
-        <TarjetaFruta name='naranja' price={5.00} />
-        <TarjetaFruta name='melon' price={1.00} />
-        <TarjetaFruta name='mango' price={3.00} />
+        <Contador/>
     </div>
 )
 
