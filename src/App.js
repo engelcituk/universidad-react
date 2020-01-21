fd997fcfdb049e001eca670f7af964c72505dfea
citkcaamal <programaciondyn3.mx@sandos.com>
2020-01-21 12:10:53 -0500

    Reenvió de la prop especial ref con forwardref

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './global.css'
 // import PropTypes from 'prop-types'
 
 // import TarjetaFruta'' from './components/TarjetaFruta';
-import Chart from 'chart.js'
+// import Chart from 'chart.js'
 
 
 // const App = () => (
@@ -518,78 +518,101 @@ import Chart from 'chart.js'
 // }
 
 // Integrar librerías de terceros usando las refs de React
-class Graficas extends Component{
+// class Graficas extends Component{
     
-    grafica = React.createRef()
+//     grafica = React.createRef()
+
+//     componentDidMount () {
+//         const ctx = this.grafica.current.getContext("2d");
+
+//          new Chart(ctx, {
+//           type: "bar",
+//           data: {
+//             labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//             datasets: [
+//               {
+//                 label: "# of Votes",
+//                 data: [12, 19, 3, 5, 2, 3],
+//                 backgroundColor: [
+//                   "rgba(255, 99, 132, 0.2)",
+//                   "rgba(54, 162, 235, 0.2)",
+//                   "rgba(255, 206, 86, 0.2)",
+//                   "rgba(75, 192, 192, 0.2)",
+//                   "rgba(153, 102, 255, 0.2)",
+//                   "rgba(255, 159, 64, 0.2)"
+//                 ],
+//                 borderColor: [
+//                   "rgba(255, 99, 132, 1)",
+//                   "rgba(54, 162, 235, 1)",
+//                   "rgba(255, 206, 86, 1)",
+//                   "rgba(75, 192, 192, 1)",
+//                   "rgba(153, 102, 255, 1)",
+//                   "rgba(255, 159, 64, 1)"
+//                 ],
+//                 borderWidth: 1
+//               }
+//             ]
+//           },
+//           options: {
+//             scales: {
+//               yAxes: [
+//                 {
+//                   ticks: {
+//                     beginAtZero: true
+//                   }
+//                 }
+//               ]
+//             }
+//           }
+//         });
+//     }
+
+//     render (){
+//         return (
+//           <div>
+//             <canvas 
+//                 ref={this.grafica}
+//                 width="400"
+//                 height="400"
+//             >
+
+//             </canvas>
+//           </div>
+//         );
+//     }
+// }
+// class App extends Component {
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>React refs libs de terceros</h1>
+//                 <Graficas />
+//             </div>
+//         )
+//     }
+// }
+
+//  Reenvió de la prop especial ref con forwardref, solo funciona para componentes funcionales
+const FancyInput = React.forwardRef( (props, ref) => (
+  <div>
+    <input type="text" ref={ref} />
+  </div>
+))
+
+class App extends Component {
+    entrada = React.createRef()
 
     componentDidMount () {
-        const ctx = this.grafica.current.getContext("2d");
-
-         new Chart(ctx, {
-          type: "bar",
-          data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [
-              {
-                label: "# of Votes",
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(255, 206, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)"
-                ],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)"
-                ],
-                borderWidth: 1
-              }
-            ]
-          },
-          options: {
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ]
-            }
-          }
-        });
+        console.log(this.entrada)
     }
-
-    render (){
-        return (
-          <div>
-            <canvas 
-                ref={this.grafica}
-                width="400"
-                height="400"
-            >
-
-            </canvas>
-          </div>
-        );
-    }
-}
-class App extends Component {
-
     render() {
         return (
-            <div>
-                <h1>React refs libs de terceros</h1>
-                <Graficas />
-            </div>
-        )
+          <div>
+            <h1>Reenvío de refs</h1>
+            <FancyInput ref={this.entrada} />
+          </div>
+        );
     }
 }
 export default App
